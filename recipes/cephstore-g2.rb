9b23b8e451bae33ab03b711637db4aae121e19f6
Kyle Bader <kyle.bader@dreamhost.com>
2013-04-13 01:11:16 -0700

    Array elements cant be created with indexs
    Need to define this way in order to add elements to an array, you can't
    explicity define the array index when setting the attribute.

--- recipes/cephstore-g2.rb
+++ recipes/cephstore-g2.rb
@@ -7,63 +7,77 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.normal['ceph']['osd-devices'][0]['device'] = '/dev/sdb'
-node.normal['ceph']['osd-devices'][0]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][0]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][0]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][1]['device'] = '/dev/sdc'
-node.normal['ceph']['osd-devices'][1]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][1]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][1]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][2]['device'] = '/dev/sdd'
-node.normal['ceph']['osd-devices'][2]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][2]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][2]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][3]['device'] = '/dev/sde'
-node.normal['ceph']['osd-devices'][3]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][3]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][3]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][4]['device'] = '/dev/sdf'
-node.normal['ceph']['osd-devices'][4]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][4]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][4]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][5]['device'] = '/dev/sdg'
-node.normal['ceph']['osd-devices'][5]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][5]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][5]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][6]['device'] = '/dev/sdh'
-node.normal['ceph']['osd-devices'][6]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][6]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][6]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][7]['device'] = '/dev/sdi'
-node.normal['ceph']['osd-devices'][7]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][7]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][7]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][8]['device'] = '/dev/sdj'
-node.normal['ceph']['osd-devices'][8]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][8]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][8]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][9]['device'] = '/dev/sdk'
-node.normal['ceph']['osd-devices'][9]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][9]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][9]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][10]['device'] = '/dev/sdl'
-node.normal['ceph']['osd-devices'][10]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][10]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][10]['status'] = 'zapdisk'
-
-node.normal['ceph']['osd-devices'][11]['device'] = '/dev/sdm'
-node.normal['ceph']['osd-devices'][11]['filesystem'] = 'xfs'
-node.normal['ceph']['osd-devices'][11]['encrypted'] = true 
-node.normal['ceph']['osd-devices'][11]['status'] = 'zapdisk'
-
+node.normal['ceph']['osd-devices'] = [
+  {
+    'device' => '/dev/sdb',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdc',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdd',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sde',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdf',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdg',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdh',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdi',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdj',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdk',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdl',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  },
+  {
+    'device' => '/dev/sdm',
+    'filesystem' => 'xfs',
+    'encrypted' => true,
+    'status' => 'zapdisk'
+  }
+]
